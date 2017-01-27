e64375cb6a27405af2878dd25d3df5ad0b673f84
j-tyler <justin.marsh@holbertonschool.com>
2017-01-27 21:56:21 +0000

    update

--- pyth.py
+++ pyth.py
@@ -3,17 +3,40 @@ import sys
 import re
 file = open(sys.argv[1], 'r') 
 l = []
-for index, line in enumerate(file):
+index = 1
+for line in file:
     if (re.findall(r'\s*push\s', line)):
         if not (re.findall(r'\s*push\s*\d', line)):
-            print("L{}: usage: push integer".format(index))
+            print("L{:d}: usage: push integer".format(index))
             sys.exit(2)
         l.insert(0, int(re.findall(r'\b\d\b', line)[0]))
     elif (re.findall(r'\s*pall\s', line)):
         print('{}'.format('\n'.join([str(i) for i in l])))
+    elif (re.findall(r'\s*pint\s', line)):
+        if len(l) == 0:
+            print("L{:d}: can't pint, stack empty".format(index))
+            sys.exit(3)
+        print('{:d}'.format(l[0]))
+    elif (re.findall(r'\s*pop\s', line)):
+        if len(l) == 0:
+            print("L{:d}: can't pop an empty stack".format(index))
+            sys.exit(4)
+        l.pop(0)
+    elif (re.findall(r'\s*swap\s', line)):
+        if len(l) < 2:
+            print("L{:d}: can't swap, stack too short".format(index))
+            sys.exit(5)
+        l[0], l[1] = l[1], l[0]
+    elif (re.findall(r'\s*add\s', line)):
+        if len(l) < 2:
+            print("L{:d}: can't add, stack too short".format(index))
+            sys.exit(6)
+        l[1] += l[0]
+        l.pop(0)
     else:
         print("L{}: unknown instruction {}".format(index,
-            str(re.findall(r'\b[^\W\d_]+\b', line)[0])))
+              str(re.findall(r'\b[^\W\d_]+\b', line)[0])))
         sys.exit(1)
+    index += 1
 sys.exit(0)
 
